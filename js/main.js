0cf2e01cbfab3632c9ad107b200b05fd0998d07a
RainbowComet <cwitw@mail.ru>
2020-06-07 16:06:47 +0300

    Работа с меню. Начала создавать галерею

---
+++ js/main.js
@@ -0,0 +1,82 @@
+
+//меню
+//галерея
+$('#menu__item').hover(
+	function(){ $('#menu__content').addClass('hover') },
+);
+$('#menu__item').hover(
+	function(){ $(this).addClass('menu_hover') },
+	function(){ $(this).removeClass('menu_hover') }
+);
+$('#menu__content').hover(
+	function(){ $('#menu__item').addClass('menu_hover') },
+	function(){ $('#menu__item').removeClass('menu_hover') }
+);
+$('#menu__content').hover(
+	function(){ $(this).addClass('hover') },
+	function(){ $(this).removeClass('hover') },
+);
+//услуги
+$('#menu__item2').hover(
+	function(){ $('#menu__content2').addClass('hover') },
+);
+$('#menu__item2').hover(
+	function(){ $(this).addClass('menu_hover') },
+	function(){ $(this).removeClass('menu_hover') }
+);
+$('#menu__content2').hover(
+	function(){ $('#menu__item2').addClass('menu_hover') },
+	function(){ $('#menu__item2').removeClass('menu_hover') }
+);
+$('#menu__content2').hover(
+	function(){ $(this).addClass('hover') },
+	function(){ $(this).removeClass('hover') },
+);
+//магазин
+$('#menu__item3').hover(
+	function(){ $('#menu__content3').addClass('hover') },
+);
+$('#menu__item3').hover(
+	function(){ $(this).addClass('menu_hover') },
+	function(){ $(this).removeClass('menu_hover') }
+);
+$('#menu__content3').hover(
+	function(){ $('#menu__item3').addClass('menu_hover') },
+	function(){ $('#menu__item3').removeClass('menu_hover') }
+);
+$('#menu__content3').hover(
+	function(){ $(this).addClass('hover') },
+	function(){ $(this).removeClass('hover') },
+);
+//форум
+$('#menu__item4').hover(
+	function(){ $('#menu__content4').addClass('hover') },
+);
+$('#menu__item4').hover(
+	function(){ $(this).addClass('menu_hover') },
+	function(){ $(this).removeClass('menu_hover') }
+);
+$('#menu__content4').hover(
+	function(){ $('#menu__item4').addClass('menu_hover') },
+	function(){ $('#menu__item4').removeClass('menu_hover') }
+);
+$('#menu__content4').hover(
+	function(){ $(this).addClass('hover') },
+	function(){ $(this).removeClass('hover') },
+);
+//о нас
+$('#menu__item5').hover(
+	function(){ $('#menu__content5').addClass('hover') },
+);
+$('#menu__item5').hover(
+	function(){ $(this).addClass('menu_hover') },
+	function(){ $(this).removeClass('menu_hover') }
+);
+$('#menu__content5').hover(
+	function(){ $('#menu__item5').addClass('menu_hover') },
+	function(){ $('#menu__item5').removeClass('menu_hover') }
+);
+$('#menu__content5').hover(
+	function(){ $(this).addClass('hover') },
+	function(){ $(this).removeClass('hover') },
+);
