9c53a0e31045460157b0d947be97d44fd1e5b82f
RainbowComet <cwitw@mail.ru>
2020-06-20 15:48:09 +0300

    Пункт меню "О нас", кнопки для магазина, Услуги

--- js/main.js
+++ js/main.js
@@ -21,11 +21,11 @@ $('#js-menu_item2').hover(
 //магазин
 $('#js-menu_item3').hover(
 	function(){ $('#menu__item3').addClass('menu_hover') },
-	function(){ $('#menu__item3').removeClass('menu_hover') }
+	function(){ $('#menu__item3').removeClass('menu_hover');/* $('.shop__radio-btn').removeAttr("checked") */}
 	);
 $('#js-menu_item3').hover(
 	function(){ $('#menu__content3').addClass('hover') },
-	function(){ $('#menu__content3').removeClass('hover') }
+	function(){ $('#menu__content3').removeClass('hover'); /*$('.shop__radio-btn').removeAttr("checked") */}
 );
 //форум
 $('#js-menu_item4').hover(
@@ -39,21 +39,23 @@ $('#js-menu_item4').hover(
 //о нас
 $('#js-menu_item5').hover(
 	function(){ $('#menu__item5').addClass('menu_hover') },
-	function(){ $('#menu__item5').removeClass('menu_hover') }
+	function(){ $('#menu__item5').removeClass('menu_hover');$("body").css("overflow", "auto"); }
 	);
 $('#js-menu_item5').hover(
 	function(){ $('#menu__content5').addClass('hover') },
-	function(){ $('#menu__content5').removeClass('hover') }
+	function(){ $('#menu__content5').removeClass('hover');$("body").css("overflow", "auto"); }
 );
 //выезжающее меню при клике
 function open_menu() {
 	(function(){ $('#main-nav').addClass('visible animate__animated animate__slideInUp') })();
 	(function(){ $('#main-nav').removeClass('hidden') }) ();
+	$("#otherpage__nav").attr("style", "height:200px;");
 	$("#openmenu").replaceWith('<li class="gallery-nav__item" id="closemenu" onclick="close_menu()"><img class="open_menu" src="img/arrows-down.svg"></li>');
 }
 function close_menu() {
 	(function(){ $('#main-nav').addClass('hidden') })();
 	(function(){ $('#main-nav').removeClass('visible') }) ();
+	$("#otherpage__nav").attr("style", "");
 	$("#closemenu").replaceWith('<li class="gallery-nav__item" id="openmenu" onmouseover="open_menu()"><img class="open_menu" src="img/arrows.svg"></li>');
 }
 function enlarge1(img) {
@@ -64,7 +66,8 @@ function enlarge1(img) {
 	$("#img_number").attr("value", img);
 }
 function change_img(next_or_prev) {
-	let num = $("#img_number").attr("value");
+	let num;
+	num = $("#img_number").attr("value");
 	let new_num;
 	let max_num_of_imgs=6; // макс количество изображений
 	if (next_or_prev=='next') {
@@ -88,4 +91,35 @@ function(){ change_img('prev') }
 function lessen() {
 	(function(){ $('#gallery').removeClass('hidden') })();
 	$("#show_img").attr("class", "show_img d-flex hidden");
+}
+
+/*смена языков анимация*/
+function activate_language_box() {
+	(function(){ $('#language_box').addClass('activated') })();
+}
+function animation(lang) {
+	activate_language_box();
+	$('span').removeClass('animationRU-en');
+	$('span').removeClass('animationDE-en');
+	$('span').removeClass('animationEN-en');
+	$('span').removeClass('animationRU-ru');
+	$('span').removeClass('animationDE-ru');
+	$('span').removeClass('animationEN-ru');
+	$('span').removeClass('animationRU-de');
+	$('span').removeClass('animationDE-de');
+	$('span').removeClass('animationEN-de');
+	(function(){ $('#en').addClass('animation'+lang+'-en') })();
+	(function(){ $('#de').addClass('animation'+lang+'-de') })();
+	(function(){ $('#ru').addClass('animation'+lang+'-ru') })();
+	
+}
+function enlarge_text (div_id) {
+	$('#'+ div_id).addClass('enlarged_text');
+	$("body").css("overflow", "hidden");
+	$('#'+ div_id).children('.loupe').attr("onclick", "lessen_text('"+div_id+"')");
+}
+function lessen_text (div_id) {
+	$('#'+ div_id).removeClass('enlarged_text');
+	$("body").css("overflow", "auto");
+	$('#'+ div_id).children('.loupe').attr('onclick', "enlarge_text('" + div_id + "')");
 }
